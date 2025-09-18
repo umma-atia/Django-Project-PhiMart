b3dfff64c009038da29ce4a6d5b10a47ddf797a2
Umma Atia <red910630@gmail.com>
2025-09-18 20:10:24 +0600

    Fronted URL added in Djoser

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -182,6 +182,9 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
+    'EMAIL_FRONTEND_PROTOCOL': config('FRONTEND_PROTOCOL'),
+    'EMAIL_FRONTEND_DOMAIN': config('FRONTEND_DOMAIN'),
+    'EMAIL_FRONTEND_SITE_NAME': 'PhiMart',
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
