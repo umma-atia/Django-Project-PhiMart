2ed892aabbf995ddb29ac0911b26539c2b007e4d
Umma Atia <red910630@gmail.com>
2025-09-19 19:03:05 +0600

    is_staff added in UserSerializer

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from product.models import Product, Category, Review
+from product.models import Product, Category, Review, ProductImage
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(ProductImage)
 admin.site.register(Category)
 admin.site.register(Review)
